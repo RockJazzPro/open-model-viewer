aad9200173ab363466db2351352d51ae0952945e
RockJazzPro <nicola.studer2000@gmail.com>
2019-01-09 15:51:45 +0100

    Create base windowing application

---
+++ src/Main.cpp
@@ -0,0 +1,63 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow * window);
+
+const unsigned int WIDTH = 1000;
+const unsigned int HEIGHT = 700;
+
+int main() {
+
+  // initialization and configuration of glfw
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Create window
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "open-model-viewer", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // load all OpenGL functions
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Faileld to initialize glad" << std::endl;
+    return -1;
+  }
+
+  //set first viewport
+  glViewport(0, 0, WIDTH, HEIGHT);
+
+  std::cout << "Hello World" << std::endl;
+  // render
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glClearColor(.1f, .1, .1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwSwapBuffers(window);
+  glfwPollEvents();
+
+  glfwTerminate();
+  return 0;
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}

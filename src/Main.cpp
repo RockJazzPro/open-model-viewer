d1d1b6d776a5b301706a0a58097ed14d47c98e3e
RockJazzPro <nicola.studer2000@gmail.com>
2019-01-21 14:46:27 +0100

    Add basic context

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <fstream>
+#include <iostream>
 #include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -13,7 +15,6 @@ const unsigned int WIDTH = 1000;
 const unsigned int HEIGHT = 700;
 
 int main() {
-
   // initialization and configuration of glfw
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -39,12 +40,95 @@ int main() {
   //set first viewport
   glViewport(0, 0, WIDTH, HEIGHT);
 
+  // center object vertices
+  float vertices[] = {
+    // Coordinates         // Colors
+    -0.5f,  0.5f,  0.2f,   1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.2f,   0.0f, 1.0f, 0.0f,
+     0.0f, -0.5f,  0.2f,   0.0f, 0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 1.0f,
+     0.5f,  0.5f, -0.2f,   0.0f, 1.0f, 0.0f,
+     0.0f, -0.5f, -0.2f,   1.0f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+
+     0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+
+     0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+     0.0f, -0.5f, -0.2f,   0.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.2f,   0.0f, 0.0f, 0.0f,
+  };
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // position attributes
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // color attributes
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // Create Shader
+  Shader myShader("vertexshader.vs", "fragmentshader.fs");
+
+  // Enable depth
+  glEnable(GL_DEPTH_TEST);
+
+  // Set projection matrix
+  myShader.use();
+  glm::mat4 projection = glm::mat4(1.0f);
+  projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+  myShader.setMat4("projection", projection);
+
   // render
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
+
+    // Background
     glClearColor(.1f, .1, .1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
+    myShader.use();
+
+    // create transformations
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+
+    model = glm::rotate(model, (float)glfwGetTime()*2.5f, glm::vec3(0.0f, 1.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+    // myShader.setMat4("model", model);
+    glUniformMatrix4fv(glGetUniformLocation(myShader.progID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    myShader.setMat4("view", view);
+
+    // render container
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 24);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }

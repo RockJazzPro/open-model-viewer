ba54565055adc4d0ad2159f1eb6cb6ee8a8d9dc1
RockJazzPro <nicola.studer2000@gmail.com>
2019-02-04 10:42:59 +0100

    Update shaders

--- src/fragmentshader.fs
+++ src/fragmentshader.fs
@@ -1,9 +1,11 @@
 #version 330 core
-out vec3 FragColor;
+out vec4 FragColor;
 
-in vec3 theColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture_diffuse1;
 
 void main()
 {
-  FragColor = theColor;
+  FragColor = texture(texture_diffuse1, TexCoord);
 }
